e575efc3ef144e45e226ed93f4affca8423569e3
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-18 23:00:18 +0100

    feat: Add conversion of enums to string when serializing. This is necessary because the frontend is using a string to represent the type and the api is using an enum.

---
+++ Enums/ShipType.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace api.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ShipType
+{
+    destroyer,
+    submarine,
+    carrier,
+    frigate,
+    battleship,
+}
