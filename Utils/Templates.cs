2d5537b1b75866260e1f06de5d8a8d09f166f352
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-19 17:28:56 +0100

    feat: add a templates class for returning an array of ships. Can add other templates later.

---
+++ Utils/Templates.cs
@@ -0,0 +1,47 @@
+﻿using api.Enums;
+using api.Models;
+
+namespace api.Utils;
+
+public struct Templates()
+{
+    public readonly ShipData[] Ships =
+    [
+        new ShipData()
+        {
+            NumberOfSections = 2,
+            Orientation = [1, 0],
+            Pos = new Position(0, 0),
+            Type = ShipType.destroyer,
+            SectionStatus = [ShipSectionStatus.ok, ShipSectionStatus.ok]
+        },new ShipData()
+        {
+            NumberOfSections = 3,
+            Orientation = [1, 0],
+            Pos = new Position(0, 0),
+            Type = ShipType.submarine,
+            SectionStatus = [ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok]
+        },new ShipData()
+        {
+            NumberOfSections = 3,
+            Orientation = [1, 0],
+            Pos = new Position(0, 0),
+            Type = ShipType.frigate,
+            SectionStatus = [ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok]
+        },new ShipData()
+        {
+            NumberOfSections = 4,
+            Orientation = [1, 0],
+            Pos = new Position(0, 0),
+            Type = ShipType.battleship,
+            SectionStatus = [ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok]
+        },new ShipData()
+        {
+            NumberOfSections = 5,
+            Orientation = [1, 0],
+            Pos = new Position(0, 0),
+            Type = ShipType.carrier,
+            SectionStatus = [ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok, ShipSectionStatus.ok]
+        },
+    ];
+}
