a899499ce7d94821d3bc18dc763f48e5af4df03a
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-23 00:54:37 +0100

    refactor: Reorder the functions.
    refactor: Change the RequestNewSession event so that the caller doesn't automatically join the requested session.
      - Independent steps will make it easier to debug and test each step later on.
    
    refactor: rename JoinExistingSession to JoinSession, for clarity.
    refactor: add functionality to the join session method to on successful session join:
     - return client and game state;
     - add the caller to the session's signalr group.
     - on both players having joined the session, move the game state forward and send appropriate game data to the clients.

--- Hubs/GameHub.cs
+++ Hubs/GameHub.cs
@@ -1,3 +1,4 @@
+using api.Enums;
 using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.SignalR;
@@ -9,11 +10,43 @@ public class GameHub(IGameSessionManager gameSessionManager) : Hub
 {
     //
     private readonly IGameSessionManager _gameSessionManager = gameSessionManager;
-
+    
     // Store currently connected user Ids in a HashSet so that we can keep count of how many users are connected currently.
     private static readonly HashSet<string> ConnectedUsers = [];
     
-    // Handle the host client requesting a new session. Return the session ID and prepare empty boards.
+    // Connections
+    public override async Task OnConnectedAsync()
+    {
+        // Add the new connection to the list of connected users.
+        ConnectedUsers.Add(Context.ConnectionId);
+        
+        // Log this connection
+        Console.WriteLine($"User has connected to server: {Context.ConnectionId}");
+        
+        // Log number of users connected.
+        Console.WriteLine($"We now have {ConnectedUsers.Count} users connected to the server");
+        
+        await base.OnConnectedAsync();
+    }
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        // Remove the connection ID from the list of connected users.
+        ConnectedUsers.Remove(Context.ConnectionId);
+        
+        // Log this connection
+        Console.WriteLine($"User has disconnected from server: {Context.ConnectionId}");
+        
+        // Log number of users connected.
+        Console.WriteLine($"We now have {ConnectedUsers.Count} users connected to the server");
+
+        // Attempt to disconnect the client from any sessions they are connected to.
+        _gameSessionManager.LeaveSession(Context.ConnectionId);        
+        
+        await base.OnDisconnectedAsync(exception);
+    }
+    
+    // Sessions
+    // Handle a client requesting a new session. Return the session ID and prepare empty boards.
     public async Task RequestNewSession(string[] rowTags, string[] colTags)
     {
         // Obtain a game session. Could be new or from a pool.
@@ -24,62 +57,61 @@ public class GameHub(IGameSessionManager gameSessionManager) : Hub
             return;
         }
         
-        // Join the host to a SignalR Group with the session ID as the group name.
-        await Groups.AddToGroupAsync(Context.ConnectionId, session.Id);
-
-        Console.WriteLine($"Sending Session Id: {session.Id}");
         // Return the session ID to the host client.
-        await Clients.Client(Context.ConnectionId)
+        Console.WriteLine($"Sending Session Id: {session.Id}");
+        await Clients.Caller
             .SendAsync("ReceiveSessionId", session.Id);
-        
+
+        // 
         Console.WriteLine($"There are currently {_gameSessionManager.GetSessionCount()} sessions ongoing.");
     }
 
-    // Handle a client request to join an existing session by session Id.
-    public async Task JoinExistingSession(string sessionId)
+    // Handle a client request to join an existing session by session id.
+    public async Task JoinSession(string sessionId)
     {
-        // Try getting the session by id.
-        var session = _gameSessionManager.GetSessionById(sessionId);
+        // Try to join the session with sessionId
+        var session = _gameSessionManager.JoinSession(sessionId, Context.ConnectionId);
         if (session == null )
         {
             // Send an error to the caller if the session doesn't exist.
-            await Clients.Caller.SendAsync("Error", "Session Not Found");
+            Console.WriteLine($"Couldn't join Session: {sessionId}");
+            await Clients.Caller.SendAsync("SessionNotFound");
             return;
         }
+        // Send game and player states to the client
+        var player = session.GameData.Players.FirstOrDefault((p)=>p.Id == Context.ConnectionId);
+        if (player == null) return; // This would be a very strange error.
+        await Clients.Caller.SendAsync("ClientStateUpdate", player.ClientState);
+        await Clients.Caller.SendAsync("GameStateUpdate", session.GameState);
         
-        //        
-        if (session.GameState.Players[0].Id == Context.ConnectionId)
+        // Join the client to the session's group
+        await Groups.AddToGroupAsync(Context.ConnectionId, session.Id);
+        
+        // Check if session is complete.
+        if (session.GameData.Players.All(p => p.Id != null))
         {
-            // The caller is the Host. Log the host joining the session.
-            Console.WriteLine($"Host joined Session Id: {session.Id}");
-        } 
-        else if (session.GameState.Players[1].Id == null )
-        {
-            // The Caller is a Guest.
-            Console.WriteLine($"Guest joined Session Id: {session.Id}");
-            
-            // Update Game state to reflect the Guest has joined the session.
-            session.GameState.Players[1].Id = Context.ConnectionId;
-            
-            // Join the guest to the same SignalR group as the host
-            await Groups.AddToGroupAsync(Context.ConnectionId, session.Id);
-        }
-
-        //
-        if (session.GameState.Players.All(player=>player.Id != null))
-        {
-            // Both clients have joined the session. Send empty boards and trigger the game setup stage on both clients.
-            await Clients.Groups(session.Id)
-                .SendAsync("BeginGameSetup", 
-                    session.GameState.RowTags, 
-                    session.GameState.ColTags, 
-                    session.GetEmptyBoard(),
-                    session.ShipPool                     
-                    );
+            if(session.GameState==EGameState.GameOver)
+            {
+                // Set the states.
+                foreach (var client in session.GameData.Players) client.ClientState = EClientState.FleetSetup;
+                session.GameState = EGameState.FleetSetup;
+                Console.WriteLine("Setting state to Fleet Setup");
+            }
+                                
+            // Send the state.
+            await Clients.Groups(sessionId).SendAsync("GameStateUpdate", session.GameState);
+            foreach (var client in session.GameData.Players)
+            {
+                await Clients.Client(client.Id!).SendAsync("ClientStateUpdate", client.ClientState);
+            }
         }
     }
     
-    public async Task CloseSession(string sessionId)
+    // Handle a client requesting to leave a session they are a part of.
+    public void LeaveSession() => _gameSessionManager.LeaveSession(Context.ConnectionId);
+    
+    /* Handle a request for initializing the Fleet setup. */
+    public async Task FleetSetup(string sessionId)
     {
         var session = _gameSessionManager.RemoveSession(sessionId);
         if(session==null) 
